6679091eb632533c2671f69498a4693e374ce6a9
Vinícius Melesque <101480393+vinimelesque@users.noreply.github.com>
2025-03-23 23:03:07 -0300

    add new main

---
+++ src/js/main.js
@@ -0,0 +1,113 @@
+/*=============== SCROLL HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header');
+    if (window.scrollY >= 50) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader);
+
+let swiperPopular = new Swiper('.popular__container', {
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+});
+
+/*=============== SWIPER ===============*/
+
+/*=============== TOGGLE ITEM ===============*/
+function toggleItem(header) {
+    const content = header.nextElementSibling;
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+}
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+// Função para ativar o link da seção visível
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 60; // Compensa a altura do cabeçalho ou margem
+        const sectionId = current.getAttribute('id');
+        const navLink = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        // Verifique se o navLink existe antes de tentar adicionar ou remover classes
+        if (navLink) {
+            if (scrollY >= sectionTop && scrollY <= sectionTop + sectionHeight) {
+                // Remova a classe de todos os links
+                document.querySelectorAll('.nav__menu a').forEach(link => {
+                    link.classList.remove('active-link');
+                });
+                // Adiciona a classe 'active-link' ao link correspondente à seção visível
+                navLink.classList.add('active-link');
+            } else {
+                // Remove 'active-link' se não estiver na seção visível
+                navLink.classList.remove('active-link');
+            }
+        }
+    });
+}
+
+// Adiciona o evento de rolagem
+window.addEventListener('scroll', scrollActive);
+
+
+/*=============== SCROLL REVEAL ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true
+})
+
+// SHOW SCROLL UP
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+// Seletores corretos para ScrollReveal
+sr.reveal(`.home__title, popular__container`)
+sr.reveal(`.home__description`, { delay: 500 })
+sr.reveal(`.home__value`, { delay: 700 })
+sr.reveal(`.popular__container`, { delay: 800, origin: 'bottom' })
+sr.reveal(`.home__value`, { delay: 700 })
+sr.reveal(`.value__images`, { origin: 'left' })
+sr.reveal(`.value__content`, { origin: 'right' })
+sr.reveal(`.subscribe__container`, { delay: 800, origin: 'bottom' })
+sr.reveal(`.contact__data`, { origin: 'left' })
+sr.reveal(`.team__container`, { delay: 800, origin: 'top' })
+sr.reveal(`.section__title`, { origin: 'left' })
+sr.reveal(`.inspiracao-imagens`, { delay: 800, origin: 'bottom' })
+sr.reveal(`.inspiracao-certificado`, { delay: 800, origin: 'bottom' })
+sr.reveal(`.contact__card`, { delay: 800, origin: 'bottom' })
+sr.reveal(`.logos__container`, { delay: 800, origin: 'bottom' })
+
+/*=============== MODAL ===============*/
+function closeModal() {
+    document.getElementById('languageModal').style.display = 'none';
+}
+
+function changeToEnglish() {
+    window.location.href = 'index-en.html'; // Altere para o link correto da versão em inglês
+}
+
+document.getElementById('languageModal').addEventListener('click', function (event) {
+    if (event.target === this) { // Verifica se o clique foi no fundo (fora da modal-content)
+        closeModal();
+    }
+});
+
